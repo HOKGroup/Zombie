1ab36859c9d8444bf1eca9438118fc71291c058b
ksobon <ksobon1986@gmail.com>
2018-07-09 11:13:59 -0400

    implemented password box

--- Zombie/Utilities/Wpf/Converters.cs
+++ Zombie/Utilities/Wpf/Converters.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
